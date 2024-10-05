b4834d02a61801026d25f9216d0a68d9204450e7
m4yoNICE <cenizaroswellrey@gmail.com>
2024-10-05 22:41:47 +0800

    done at home 10/05/24 done model, factory, seeders.. not sure if theyre right but ye

---
+++ database/seeders/TodoSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\TodoModel; 
+
+class TodoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        TodoModel::factory()->count(10)->create();
+    }
+}
